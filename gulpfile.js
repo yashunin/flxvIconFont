61f7dc8408cf11174a5cffafe287c9e309c386b2
wwwcoderwww <wwwcoderwww@gmail.com>
2020-01-24 02:03:32 +0200

    Создал html страницу со всеми иконками, оставил только шрифт .woff, переименовал иконки

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,46 @@ var fs = require('fs');
 gulp.task('Iconfont', function (done) {
 
   var allIcons = []
+  var htmlIcons = []
   fs.readdir('icons', function (err, items) {
     console.log(items);
 
     for (var i = 0; i < items.length; i++) {
-      allIcons.push('icons/' + items[i])
-      console.log(items[i]);
+      if (items[i].match('.svg'))
+        allIcons.push('icons/' + items[i])
+      htmlIcons.push(items[i].substring(0, items[i].length - 4) + ' flxvIcon')
+      console.log(items[i].substring(0, items[i].length - 4));
     }
 
     webfontsGenerator({
       files: allIcons,
       fontHeight: 1000,
       templateOptions: {
-        classPrefix: 'iconfont-',
-        baseSelector: '.iconfont'
+        classPrefix: 'flxv',
+        baseSelector: '.flxvIcon',
+        // classPrefix: 'iconfont-',
+        // baseSelector: '.iconfont',
+        names: htmlIcons
       },
-      dest: 'dest/',
+      order: ['woff'],
+      types: ['woff'],
+      html: true,
+      dest: 'dist/',
     }, function (error) {
       if (error) {
         console.log('Fail!', error);
       } else {
         console.log('Done!');
+
+        fs.unlink('dist/iconfont.svg', (err) => {
+          console.log('dist/iconfont.svg was deleted');
+        })
+        fs.unlink('dist/iconfont.ttf', (err) => {
+          console.log('dist/iconfont.ttf was deleted');
+        })
       }
     })
+
   });
 
   done()
